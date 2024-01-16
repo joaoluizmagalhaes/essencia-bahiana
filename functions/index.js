cf04f3466e8f04b86cc120f06ab0455971615beb
Joao Luiz de Figueiredo Magalhães <dukejlf@gmail.com>
2024-01-16 15:20:39 -0300

    Created axios funcion

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,16 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const axios = require('axios');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+exports.fetchPlaces = functions.https.onCall(async (data, context) => {
+  const apiKey = 'SUA_CHAVE_API'; // Substitua pela sua chave de API do Google Places
+  const { location, radius, type } = data; // Dados recebidos do app cliente
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=${type}&key=${apiKey}`;
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  try {
+    const response = await axios.get(url);
+    return response.data.results; // Retorna os resultados da API
+  } catch (error) {
+    console.error('Erro ao buscar locais:', error);
+    throw new functions.https.HttpsError('internal', 'Erro ao buscar locais');
+  }
+});
