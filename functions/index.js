0188c433971fbd25963267818e9b2a25fd2b63f6
Joao Luiz de Figueiredo Magalhães <dukejlf@gmail.com>
2024-01-16 16:45:15 -0300

    Fized functions.

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,12 @@ const functions = require('firebase-functions');
 const cors = require('cors')({origin: true})
 const axios = require('axios');
 
-eexports.fetchPlaces = functions.https.onRequest((request, response) => {
+exports.fetchPlaces = functions.https.onRequest((request, response) => {
   cors(request, response, async () => {
     // Verifique se o método é GET
     if (request.method !== 'GET') {
       return response.status(403).send('Método HTTP não suportado');
     }
-    console.log('here')
     const apiKey = 'AIzaSyDSdTrDltNnwQ1l5Fuo5jhHiGcBsBf_0Sk'; // Substitua pela sua chave de API do Google Places
     const { location, radius, type } = data; // Dados recebidos do app cliente
     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=${type}&key=${apiKey}`;
