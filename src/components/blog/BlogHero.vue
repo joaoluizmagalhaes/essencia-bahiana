7618f36ce53d213f7a3f2bbca43f1cda5bd4980b
Joao Luiz de Figueiredo Magalhães <dukejlf@gmail.com>
2024-01-19 13:11:00 -0300

    Added new items on blog home.

--- src/components/blog/BlogHero.vue
+++ src/components/blog/BlogHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mx-auto container flex flex-col md:flex-row md:h-[500px]">
-    <div class="w-full md:w-7/12 px-6 md:px-0 box-border flex md:h-[500px]">
+  <div class="mx-auto container flex flex-col md:flex-row md:h-[600px]">
+    <div class="w-full md:w-7/12 px-6 md:px-0 box-border flex md:h-[600px]">
       <ArticleHero :article="dataHero[0]" />
     </div>
     <div class="w-full md:w-5/12 px-6 md:px-0 mb-10 md:mb-0 box-border md:justify-between flex flex-col  md:h-[unset]">
--- src/pages/BlogHome.vue
+++ src/pages/BlogHome.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page class="blog-home">
-    <main class="my-28 md:my-48 flex flex-col">
+    <main class="my-28 md:mt-48 md:mb-10 flex flex-col">
       <BlogHero :dataHero="dataHero"/>
 
       <AdComponent :dataAdSlot="dataAdSlotBanner" />
 
-      <div class="flex container mx-auto my-20">
+      <div class="flex container mx-auto ">
         <section class="w-full md:w-9/12">
-          <ArticleBody v-for="item in dataHero" :article="item" :key="item.title" />
+          <ArticleBody v-for="item in dataBody" :article="item" :key="item.title" />
         </section>
         <aside class="w-full md:w-3/12">
           <AdComponent :dataAdSlot="dataAdSlotSquare" />
@@ -34,7 +34,7 @@ const dataHero = ref([
     imageURL: 'espelho-blog-hero.jpg',
     excerpt: 'Explore águas cristalinas e areias reluzentes. Um verdadeiro paraíso que reflete a serenidade do céu.',
     containerStyle: 'md:pr-6',
-    imageStyle: 'md:h-[500px]',
+    imageStyle: 'md:h-[600px]',
     titleStyle: 'md:text-5xl',
     excerptStyle: 'md:text-lg'
   },
@@ -43,7 +43,7 @@ const dataHero = ref([
     imageURL: 'caraiva-blog-hero.jpg',
     excerpt: 'Descubra a simplicidade encantadora de Caraíva, onde ruas de areia e casas coloridas desenham o cenário.',
     containerStyle: 'md:pb-3',
-    imageStyle: 'md:h-[238px]',
+    imageStyle: 'md:h-[288px]',
     titleStyle: 'md:text-2xl',
     excerptStyle: 'md:text-md'
   },
@@ -52,9 +52,32 @@ const dataHero = ref([
     imageURL: 'quadrado-blog-hero.jpg',
     excerpt: 'Mergulhe na elegância de Trancoso, um lugar onde praias deslumbrantes encontram um rico patrimônio cultural.',
     containerStyle: 'md:pt-3',
-    imageStyle: 'md:h-[238px]',
+    imageStyle: 'md:h-[288px]',
     titleStyle: 'md:text-2xl',
     excerptStyle: 'md:text-md'
+  }
+])
+
+const dataBody = ref([
+  {
+    title: "Descubra Arraial d'Ajuda: Um Paraíso Escondido",
+    imageURL: 'arraial-blog.jpeg',
+    excerpt: "Arraial d'Ajuda: praias deslumbrantes, cultura rica e natureza vibrante."
+  },
+  {
+    title: 'Passarela do Álcool: O Coração Noturno de Porto Seguro',
+    imageURL: 'passarela-blog.jpeg',
+    excerpt: 'A alma das noites de Porto Seguro, com bares, artesanato e muita vida.'
+  },
+  {
+    title: 'Toa Toa: Diversão e Música à Beira-Mar',
+    imageURL: 'toa-toa-blog.jpeg',
+    excerpt: 'Diversão garantida com música, dança e gastronomia à beira-mar.',
+  },
+  {
+    title: 'Axé Moi: O Palco da Alegria em Porto Seguro',
+    imageURL: 'axe-blog.jpeg',
+    excerpt: 'Axé Moi: o epicentro de festas, música ao vivo e cultura bahiana.',
   }
 ])
 
